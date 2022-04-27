b4b9b1dc105c8538594b5a1f094f0c109a991a85
arifulislam <ariful.islam42@gmail.com>
2022-04-27 13:35:19 -0400

    newproject

--- src/main/java/Scenario2.java
+++ src/main/java/Scenario2.java
@@ -1,6 +1,12 @@
 import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Scenario2 extends Reusable_Annotations {
 
@@ -10,13 +16,15 @@ public class Scenario2 extends Reusable_Annotations {
         driver.navigate().to("http://automationpractice.com");
         Reusable_Actions_Logger.clickMethod(driver,"//*[@id='block_top_menu']/ul/li[3]/a",logger,"T-Shirts");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
-        //scroll to the view by pixels
         jse.executeScript("scroll(0,600)");
-        //Reusable_Actions_Logger.mouseHover(driver,"//[contains(@href'http://automationpractice.com/index.php?id_product=1&controller=product')]",logger,"href");
-        Reusable_Actions_Logger.clickMethod(driver,"//*[@title='Add to cart']",logger,"Add To Cart");
-        Reusable_Actions_Logger.clickMethod(driver,"//*[@title='Proceed to checkout']",logger,"Proceed To Checkout");
+        Reusable_Actions_Logger.mouseHover(driver,"//*[@class='ajax_block_product col-xs-12 col-sm-6 col-md-4 first-in-line last-line first-item-of-tablet-line first-item-of-mobile-line last-mobile-line']",logger,"MouseHover");
+        Reusable_Actions_Logger.clickMethod(driver,"//*[@title='Add to cart']",logger,"Proceed to checkout");
+        Reusable_Actions_Logger.clickMethod(driver,"//*[@title='Proceed to checkout']",logger,"Proceed to checkout");
+        Reusable_Actions_Logger.clickMethod(driver,"//*[@title='Delete']",logger,"Delete");
+
         //open : http://automationpractice.com
         //Tap T-Shirts
+        //ajax_block_product col-xs-12 col-sm-6 col-md-4 first-in-line last-line first-item-of-tablet-line first-item-of-mobile-line last-mobile-line
         //Tap Add to Cart
         //Proceed to Checkout
         //Verify the count and delete the Item
